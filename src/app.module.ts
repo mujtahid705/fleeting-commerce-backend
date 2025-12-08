820c48cb2b4dd792511370d3c9f670c9dabb0cdb
Muhammad Mujtahid <mujtahid705@gmail.com>
2025-12-09 01:56:44 +0600

    jwt logic updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +10,9 @@ import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
     UsersModule,
     AuthModule,
